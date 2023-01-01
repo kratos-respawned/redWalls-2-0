ad4122637a7c21814f53164804b5cb954f7f4d3a
Gaurav Bhandari <sbhandari1907@protonmail.com>
2023-01-01 18:46:34 +0530

    migrated to next 13 and typescript

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: false,
-  images: {
-     minimumCacheTTL: 86400,
-    domains: [
-      "i.redd.it",
-      "preview.redd.it",
-      "images.pexels.com",
-      "external-preview.redd.it",
-    ],
-  },
-};
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
