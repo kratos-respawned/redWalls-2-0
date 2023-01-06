db016c503d69fb9c8710c82c55c1074b5e307aac
Gaurav Bhandari <sbhandari1907@protonmail.com>
2023-01-06 18:11:33 +0530

    modified image path

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -13,12 +13,13 @@ export default function HomeCard({ link, text, route }: WallData) {
           <div className="w-full min-h-[10rem] h-full lg:h-full relative">
             <Image
               alt={text}
-              src={link}
+              src={`/redWalls-2_0${link}`}
               placeholder="blur"
               blurDataURL={link}
               layout="fill"
               priority
               objectFit="cover"
+              unoptimized={true}
               className="group-hover:scale-105  transition-transform transform-gpu duration-500 ease-out"
             />
             <div className="bg-[#2e2e2e87] font-mono grid place-items-center text-3xl font-bold w-full h-full  aspect-auto absolute top-0 left-0">
